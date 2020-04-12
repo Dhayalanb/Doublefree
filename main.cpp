4d74feb75cce3a0b178eb47617734885d68e5a1e
Dhayalanb <coe13b006@iiitdm.ac.in>
2020-04-12 17:30:30 +0200

    Added Delete, realloc

--- main.cpp
+++ main.cpp
@@ -12,5 +12,18 @@ int main(){
     void * ptr = malloc(1337);
     free(ptr);
     free(ptr);
+
+    void * ptr_1 = malloc(1337);
+    free(ptr_1);
+    delete ptr_1;
+
+    void * ptr_2 = malloc(1337);
+    free(ptr_2);
+    Free_ptr(ptr_2);
+
+    void * ptr_3 = malloc(1337);
+    free(ptr_3);
+    realloc(ptr_3, 1337);
+
     return 0;
 }
